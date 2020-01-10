2ba555951156ea0006e8c85ce30e1893d94df42e
JayC-github <57655915+JayC-github@users.noreply.github.com>
2020-01-10 16:25:04 +1100

    Add files via upload

---
+++ lab/lab01/bird.c
@@ -0,0 +1,14 @@
+// Jay z5261536
+// Written 21/02/2019
+// by Jay CHEN
+
+#include <stdio.h>
+ 
+int main(void) {
+    printf("  ___ \n");
+    printf(" ('v')\n");
+    printf("((___))\n");
+    printf(" ^   ^\n");
+    return 0;
+
+}
